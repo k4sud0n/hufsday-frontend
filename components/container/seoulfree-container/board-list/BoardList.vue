6cb66266b6af7a6f92e868a8d67debd42463aa01
Sihyun Kim <k4sud0n@gmail.com>
2021-10-01 01:22:23 +0900

    Fix svg

--- components/container/seoulfree-container/board-list/BoardList.vue
+++ components/container/seoulfree-container/board-list/BoardList.vue
@@ -6,20 +6,7 @@
         한국외대 서울캠퍼스 자유게시판입니다.
       </div>
       <div class="flex mt-2">
-        <svg
-          class="w-5 h-5 text-green-900"
-          fill="none"
-          stroke="currentColor"
-          viewBox="0 0 24 24"
-          xmlns="http://www.w3.org/2000/svg"
-        >
-          <path
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            stroke-width="2"
-            d="M4 4v5h.582m15.356 2A8.001 8.001 0 004.582 9m0 0H9m11 11v-5h-.581m0 0a8.003 8.003 0 01-15.357-2m15.357 2H15"
-          ></path>
-        </svg>
+        <svg class="w-5 h-5 text-green-900" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path></svg>
         <svg
           class="w-5 h-5 ml-2 text-green-900"
           fill="none"
